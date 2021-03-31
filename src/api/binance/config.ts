a78b19cd62913da4723af39eaba4d33fa316e909
michal.lipinski <michal.lipinski@gmail.com>
2021-03-31 11:24:56 +0200

    x

--- src/api/binance/config.ts
+++ src/api/binance/config.ts
@@ -1,8 +1,5 @@
+import {prodKey, prodSecret} from './credentials'
 const dev = process.env.NODE_ENV !== 'production'
-const devKey = '---'
-const devSecret = '---'
-const prodKey = 'uLLfAnK5N451KqbSjx2W9MQgtK0CkvnpowPRwPRYeyBK5mX686JTxJUfGxuDXrrW' // API Key
-const prodSecret = '5Nj5mADcjRBSZ4Q9kIoJw7rHIMFaKzUjm4PXrpzt3Scdr4TCHZbWrXsy78bHjTtW' // API Private Key
 export const key = prodKey
 export const secret = prodSecret
 export const baseUrl = 'https://api.binance.com/api/v3'
@@ -10,17 +7,24 @@ export const baseCoin = 'USDT'
 export const markets = new Set(['USDT', 'BTC', 'BNB'])
 export const activePairs = new Set([
   'AUDIOUSDT',
-  'COSBTC',
-  'CELRUSDT',
-  'POLYBTC',
   'ONTUSDT',
+  'CELRUSDT',
   'SCUSDT',
-  'BNBBTC',
   'BTCUSDT',
+  'COSBTC',
+  'POLYBTC',
+  'BNBBTC',
+  'FUNBTC',
+  'CKBBTC',
+  'CNDBTC',
+  'PHBBTC',
+  'TROYBTC',
+  'CDTBTC',
+
   'ENJBNB',
 ])
 export const desiredPrice = '100' // How many $ per transaction ( min 20, recomended min 40 )
-export const desiredPriceBTC = '100' // How many $ per transaction ( min 20, recomended min 40 )
+export const desiredPriceBTC = '50' // How many $ per transaction ( min 20, recomended min 40 )
 export const desiredPriceBNB = '20' // How many $ per transaction ( min 20, recomended min 40 )
 export const minPriceDrop = 1.2
 export const takeTrailingProfitFrom = 1
@@ -32,6 +36,6 @@ export const dontBuybelow = {
   BTC: Number.parseInt(desiredPriceBTC, 10) * 5,
   BNB: Number.parseInt(desiredPriceBNB, 10) * 5,
 }
-export const safeBuyTresholds = [5, 6, 12, 20]
+export const safeBuyTresholds = [6, 6, 12, 20]
 export const safeBuyMultipliers = [1, 1, 1, 2]
 export const greed = 0.3
